cbe859a512a3758de7c8cc24750f472cf04532ac
Shourya Mittal <mittalshourya97@gmail.com>
2021-01-05 19:05:51 +0530

    Added support for arrays and objects

---
+++ flatten.js
@@ -0,0 +1,48 @@
+/******************************************************** 
+ * 
+ *  Created by : Shourya Mittal
+ *  liscense: MIT
+ *  Year: 2021
+ *  I really wish this can help you out. Happy Coding.
+ * 
+**********************************************************/
+
+
+"use strict";
+
+function flatten(obj) {
+    let flatObject = {};
+
+    for(let prop in obj) {
+        if(!obj.hasOwnProperty(prop))
+            continue;
+        if(Array.isArray(obj[prop])) {
+            //handle array
+            for(let i = 0 ; i < obj[prop].length; i++) {
+                if(Array.isArray(obj[prop][i]) || typeof obj[prop][i] == "object") {
+                    let tempFlatObj = flatten(obj[prop][i]);
+                    for(let j in tempFlatObj) {
+                        flatObject[prop + "[" + i + "]" + "." + j] = tempFlatObj[j];
+                    }
+                }
+                else {
+                    flatObject[prop + "[" + i + "]"] = obj[prop][i];
+                }
+            }
+        }
+        else if(typeof obj[prop] == "object") {
+            //handle object
+            let tempFlatObj = flatten(obj[prop]);
+            for(let i in tempFlatObj) {
+                flatObject[prop + "." + i] = tempFlatObj[i];
+            }
+        }
+        else {
+            //simple property
+            flatObject[prop] = obj[prop];
+        }
+    }
+    return flatObject;
+}
+
+module.exports = flatten;
